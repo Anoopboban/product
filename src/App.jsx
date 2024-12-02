f528edcebe457b5e4abb7016791396f54b509ac4
Your Name <you@example.com>
2024-12-02 17:28:14 +0530

    final version

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+    <div className=''>
+    <h1>Product Inventory system</h1>
+    <h2>products</h2>   
+
+    </div> 
+    </>
+  )
+}
+
+export default App
+
